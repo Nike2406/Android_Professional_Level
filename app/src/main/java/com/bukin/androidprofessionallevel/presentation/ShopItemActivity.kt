a749100c0627004bfad91d234eb47fe124eef44d
Nike2406 <locus2@mail.ru>
2022-10-19 19:42:59 +0300

    added raw second screen

---
+++ app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.bukin.androidprofessionallevel.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bukin.androidprofessionallevel.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
