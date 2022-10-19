973f80ee8b3abb49911655494af47c458648cd03
Nike2406 <locus2@mail.ru>
2022-10-19 13:08:17 +0300

    added DiffUtils realization

--- app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bukin.androidprofessionallevel.R
 import com.bukin.androidprofessionallevel.domain.ShopItem
@@ -14,16 +15,32 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
     var count = 0
     var shopList = listOf<ShopItem>()
         set(value) {
+            // Исправляем через DiffUtils
+            // Проверяем, изменились ли элементы
+            val callback = ShopListDiffCallback(shopList, value)
+            // Производим вычисления, какие элементы изменились для адапетера
+            // принимает callback
+            // ! Работает в главном потоке, т.е. главный поток ждет пока закончится оп-ция
+            val diffResult = DiffUtil.calculateDiff(callback)
+            // Чтобы адаптер применил изменения, выполняем diffResult.dispatchUpdatesTo(adapter),
+            // который уже вызовет необходимые методы (
+            // notifyItemChanged(int), notifyItemInserted(int),
+            // notifyItemRemoved(int), notifyItemRangeChanged(int, int),
+            // notifyItemRangeInserted(int, int), notifyItemRangeRemoved(int, int))
+            diffResult.dispatchUpdatesTo(this)
+            // В конце присваиваем новое занчение через сеттер
             field = value
+            /*
+            * notifyDataSetChanged() - не поределяет, что именно изменилось в списке и
+            * перерисовывает все
             notifyDataSetChanged() // обновление данных
+            * */
         }
     var onShopItemLongCLickListener: ((ShopItem) -> Unit)? = null
     var onShopItemClickListener: ((ShopItem) -> Unit)? = null
 
     // Как создать View getItemViewType()
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        Log.d("ADAPTER_ON_CREATE", "ViewHolder created, count: ${++count}")
-
         // Определяем View через
         val shopItemLayout = when (viewType) {
             VIEW_TYPE_DISABLED -> R.layout.item_shop_disabled
@@ -44,12 +61,15 @@ class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>
 
     // Как вставить значения во View
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
+        /*
+        Проблема: Адаптер привязывает все элменты при изменении состояния
+        (notifyDataSetChanged() )
+        Решение: Передавать список в адаптер для сравнения через DiffUtils,
+        который будет определять изменения
+        */
+        Log.d("ADAPTER_ON_BIND", "onBindViewHolder created, count: ${++count}")
         val shopItem = shopList[position]
         holder.view.setOnLongClickListener {
-            /*
-            * Здесь вызывается ляибда-выражение onShopItemLongCLickListener: ((ShopItem) -> Unit)?
-            * Т.к. выражение может содеражть null, то используется метод invoke()
-            * */
             onShopItemLongCLickListener?.invoke(shopItem)
             true
         }
