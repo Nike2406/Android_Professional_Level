083c95c6829d39c68ebbd5ed2f126b4e69b22eee
Nike2406 <locus2@mail.ru>
2022-10-21 11:05:10 +0300

    added new activity and ViewModel via LiveData

--- app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopItemActivity.kt
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bukin.androidprofessionallevel.R
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: ShopItemViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
+        // Из activity нельзя изменять ViewModel
+        viewModel.errorInputName.value = false
     }
 }
--- app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopItemViewModel.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/presentation/ShopItemViewModel.kt
@@ -1,5 +1,7 @@
 package com.bukin.androidprofessionallevel.presentation
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bukin.androidprofessionallevel.data.ShopListRepositoryImpl
 import com.bukin.androidprofessionallevel.domain.ShopItem
@@ -15,8 +17,39 @@ class ShopItemViewModel : ViewModel() {
     private val addShopItemUseCase = AddShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
+    // LiveData
+    /*
+    * View должна только подписываться на объекты LD и реагировать на
+    * изменения в них, но не должна сама вставлять значения
+    * Если изменить на LiveData (т.е. без сеттера), то проблема исчезнет,
+    * но есть более популярная реализация от Google с двумя переменными
+    * (private & public)
+    * */
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName // рекомендация Google
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    /*
+    * Т.к. нам нужно опопвестить экран, что его можно закрыть,
+    * а тип передаваемого значения нам не важен, то исп. Unit
+    * */
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+
+
     fun getShopItem(shopItemId: Int) {
         val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
     }
 
     fun addShopItem(inputName: String?, inputCount: String?) {
@@ -26,17 +59,20 @@ class ShopItemViewModel : ViewModel() {
         if (fieldsValid) {
             val shopItem = ShopItem(name, count, true)
             addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
         }
     }
 
     fun editShopItem(inputName: String?, inputCount: String?) {
-        // TODO(): take oldData and replace
         val name = parseName(inputName)
         val count = parseCount(inputCount)
         val fieldsValid = validateInput(name, count)
         if (fieldsValid) {
-            val shopItem = ShopItem(name, count, true)
-            editShopItemUseCase.editShopItem(shopItem)
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
         }
     }
 
@@ -52,16 +88,37 @@ class ShopItemViewModel : ViewModel() {
         }
     }
 
+    /*
+    * Согласно MVVM нельзя создать ссылку на activity из ViewModel.
+    * Вместо этого можно создать объект LiveData в VM, и кодга нужно будет отобразить
+    * ошибку, мы отправим в LD значение, Activity подпишется на нее и в нужный
+    * момент отобразит ошибку.
+    * */
     private fun validateInput(name: String, count: Int): Boolean {
         if (name.isBlank()) {
-            // TODO(): show error input name
+            _errorInputName.value = true
             return false
         }
 
-        if (count < 0) {
-            // TODO(): show error input count
+        if (count <= 0) {
+            _errorInputCount.value = true
             return false
         }
         return true
+    }
+
+    /*
+    * При повторном вводе пользователя, ошибку стоит убрать
+    * */
+    fun resetErrorInputName() {
+        _errorInputName.value =false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value =false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
     }
 }
