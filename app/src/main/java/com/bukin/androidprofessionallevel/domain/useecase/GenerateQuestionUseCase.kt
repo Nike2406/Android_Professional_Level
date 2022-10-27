e09a40f0816320711f3c7d400342eed67c46ea25
Nike2406 <locus2@mail.ru>
2022-10-27 11:44:00 +0300

    added some description

--- app/src/main/java/com/bukin/androidprofessionallevel/domain/useecase/GenerateQuestionUseCase.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/domain/useecase/GenerateQuestionUseCase.kt
@@ -7,11 +7,17 @@ class GenerateQuestionUseCase(
     private val repository: GameRepository
 ) {
 
+    /*
+    * Т.к. usecase, уже содержит имя действия, то в котлине
+    * дали возможность переопределить  метод invoke()
+    * */
     operator fun invoke(maxSumValue: Int): Question {
         return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
     }
 
     companion object {
+        // количество вараинтов - часть бизнес логики, поэтому хранится
+        // в самом usecase
         private const val COUNT_OF_OPTIONS = 6
     }
 }
