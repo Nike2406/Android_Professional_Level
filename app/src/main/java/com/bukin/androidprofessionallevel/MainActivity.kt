30ab6a00d3d53013317aba0204030071c8767baf
Nike2406 <locus2@mail.ru>
2022-12-03 12:27:27 +0300

    8.00 shown callback hell and updated gradle to 7.4.0

--- app/src/main/java/com/bukin/androidprofessionallevel/MainActivity.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.bukin.androidprofessionallevel
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import com.bukin.androidprofessionallevel.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,29 +22,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // CallbackHell
     private fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
-        val city = loadCity()
-        binding.tvLocation.text = city
-        val temp = loadTemperature(city)
-        binding.tvTemperature.text = temp.toString()
-        binding.progress.isVisible = false
-        binding.buttonLoad.isEnabled = true
+        // Код внутри коллбека будет асинхнонным, т.е.
+        // вызываться после окончания загрузки
+        loadCity { it1 ->
+            binding.tvLocation.text = it1
+            loadTemperature(it1) {
+                // Далее, новые коллбеки также будут
+                // вложенными
+                binding.tvTemperature.text = it.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+        // После коллбека код будет вызываться мговенно
+        Log.d("FUCK", "Here is Jonny!")
     }
 
-    private fun loadCity(): String {
-        Thread.sleep(5000)
-        return "Moscow"
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Moscow")
+        }
     }
 
-    private fun loadTemperature(city: String): Int {
-        Toast.makeText(
-            this,
-            getString(R.string.loading_temperature_toast, city),
-            Toast.LENGTH_SHORT
-        ).show()
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            Toast.makeText(
+                this,
+                getString(R.string.loading_temperature_toast, city),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
         Thread.sleep(5000)
-        return 17
+        callback.invoke(17)
     }
 }
