7ca042e7e31a5607505de931c82c8dd5735da99f
Nike2406 <locus2@mail.ru>
2022-12-03 13:53:37 +0300

    8.01 added handler

--- app/src/main/java/com/bukin/androidprofessionallevel/MainActivity.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bukin.androidprofessionallevel
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -22,8 +24,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    /*
+    * Handler
+    * Для того, чтобы разные потоки могли общаться друг с другом
+    * и передвать между собой данные, в android был добавлен
+    * класс Handler.
+    * Объект этого класса можно создать на главном потоке. Затем
+    * из любого потока ему можно передавать объекты Runnable и
+    * тогда метод run() будет вызван на главном потоке.
+    *
+    * Handler отправляет сообщения в Looper и, соответсвенно
+    * H не может быть вызван не из главного потока без Looper.prepare()
+    * */
+
     // CallbackHell
     private fun loadData() {
+        Log.d("MainActivity", "Load started: $this")
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
         // Код внутри коллбека будет асинхнонным, т.е.
@@ -40,24 +56,40 @@ class MainActivity : AppCompatActivity() {
         }
         // После коллбека код будет вызываться мговенно
         Log.d("FUCK", "Here is Jonny!")
+        Log.d("MainActivity", "Load finished: $this")
     }
 
     private fun loadCity(callback: (String) -> Unit) {
+        // Не главный поток
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moscow")
+            // Для явного указания потока, в handler
+            // нужно передать Looper с соотв. типом
+            runOnUiThread {
+                // Метод будет вызван в главном потоке через handler,
+                // (т.к. handler был объявлен на гл-м потоке)
+                callback.invoke("Moscow")
+            }
+            // Если H вызывать не из главного потоко,
+            // то следует использовать Looper.myLooper()?,
+            // обработать null и вызв-ть Looper.prepare()
         }
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
+            // Handler(Looper.getMainLooper()).post{} -> runOnUiThread{}
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
         Thread.sleep(5000)
-        callback.invoke(17)
+        Handler(Looper.getMainLooper()).post {
+            callback.invoke(17)
+        }
     }
 }
