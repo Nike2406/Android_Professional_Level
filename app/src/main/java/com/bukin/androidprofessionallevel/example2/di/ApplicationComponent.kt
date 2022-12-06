0ba8e2a8359bc79da55bed38263774659f22581e
Nike2406 <locus2@mail.ru>
2022-12-06 14:57:47 +0300

    11.05 added builder and factory

--- app/src/main/java/com/bukin/androidprofessionallevel/example2/di/ApplicationComponent.kt
+++ app/src/main/java/com/bukin/androidprofessionallevel/example2/di/ApplicationComponent.kt
@@ -10,21 +10,41 @@ interface ApplicationComponent  {
 
     fun inject(activity: MainActivity)
 
-   /**
-   Говорим даггеру, что будем использовать свой builder()
+//   /**
+//   Говорим даггеру, что будем использовать свой builder()
+//
+//    Этот способ предпочтительней, если нам нужно передать в граф
+//    свое значение, а после его использоваь, например context
+//
+//   @BindsInstance - помечает метод на component builder или параметр
+//    на component factory как привязку инстанса к какому-нибудь ключу
+//    внутри компонента.
+//   */
+//    @Component.Builder
+//    interface ApplicationComponentBuilder {
+//
+//        @BindsInstance // Вставляем в граф зависимостей
+//        fun context(context: Context): ApplicationComponentBuilder
+//
+//        @BindsInstance
+//        fun timeInMillis(timeInMillis: Long): ApplicationComponentBuilder
+//
+//        fun build(): ApplicationComponent
+//    }
 
-    Этот способ предпочтительней, если нам нужно передать в граф
-    свое значение, а после его использоваь, например context
-   */
-    @Component.Builder
-    interface ApplicationComponentBuilder {
+    /**
+    * В новой версии dagger появилась возможность создавать зависимости
+     * через фабрику.
+     * Если через @Component.Builder нам нужно было создавать метод,
+     * то через фабрику можно передать все параметры через create(components),
+     * их также нужно пометить @BindsInstance
+    * */
+    @Component.Factory
+    interface ApplicationComponentFactory {
 
-        @BindsInstance // Вставляем в граф зависимостей
-        fun context(context: Context): ApplicationComponentBuilder
-
-        @BindsInstance
-        fun timeInMillis(timeInMillis: Long): ApplicationComponentBuilder
-
-        fun build(): ApplicationComponent
+        fun create(
+            @BindsInstance context: Context,
+            @BindsInstance timeInMillis: Long
+        ): ApplicationComponent
     }
 }
